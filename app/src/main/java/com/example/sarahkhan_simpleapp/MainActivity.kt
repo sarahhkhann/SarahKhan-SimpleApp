3a1fc7fbab18ebff19a3e9f7d865266686194e3a
Sarah Khan <sarkhan@bu.edu>
2024-03-16 15:27:39 -0400

    added images, buttons, and edittext

--- app/src/main/java/com/example/sarahkhan_simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/sarahkhan_simpleapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.sarahkhan_simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val imageIDs = arrayOf(
+        R.drawable.beach,
+        R.drawable.desert,
+        R.drawable.jungle,
+        R.drawable.mountains
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val imageViewRandom: ImageView = findViewById(R.id.imageViewRandom)
+        val buttonShowRandomImage: Button = findViewById(R.id.imageViewButton)
+
+        buttonShowRandomImage.setOnClickListener {
+            val randomImageId = imageIDs[Random.nextInt(imageIDs.size)]
+            imageViewRandom.setImageResource(randomImageId)
+        }
+
+
     }
 }
